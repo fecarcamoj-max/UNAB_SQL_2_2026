1ea6760b90f7dcb515c1a58f87966d9ec6f3c171
ingfelipeacj <felipecarcamo@upla.cl>
2026-09-11 20:35:19 -0300

    ADD avances clase 11 09

--- sql_3_2.sql
+++ sql_3_2.sql
@@ -15,35 +15,58 @@ WHERE persona.id_persona = ventas.id_persona;
 
 # JOIN SQL...
 # Muestra las ventas con los datos de la persona. INNER JOIN (Intersección)
-SELECT * FROM ventas
-INNER JOIN persona ON persona.id_persona = ventas.id_persona;
+SELECT * FROM ventas as izq
+INNER JOIN persona as der ON der.id_persona = izq.id_persona;
 
 # LEFT JOIN (Unión a la izquierda).
-SELECT * FROM ventas
-LEFT JOIN persona ON persona.id_persona = ventas.id_persona;
+SELECT * FROM ventas as izq
+LEFT JOIN persona as der ON der.id_persona = izq.id_persona;
 
 # Right Join:::: (Unión a la derecha).
-SELECT * FROM ventas
-RIGHT JOIN persona ON persona.id_persona = ventas.id_persona;
+SELECT * FROM ventas as izq
+RIGHT JOIN persona as der ON izq.id_persona = der.id_persona;
 
-# SELF JOIN ; WHERE # Hacer parejas con personas que vivan en la misma ciudad.
+# Unión consigo mismo.
+# SELF JOIN ; WHERE 
+# PEJ: Hacer parejas con personas que vivan en la misma ciudad.
+SELECT * FROM persona as p1, persona p2
+WHERE p1.ciudad = p2.ciudad and p1.id_persona != p2.id_persona;
 
 
 # Forma óptima de hacer un SELF JOIN. (INNER JOIN)
+SELECT * FROM persona as p1
+INNER JOIN persona p2 ON p1.ciudad = p2.ciudad AND p1.id_persona <> p2.id_persona;
+#REVISION DE TABLA PERSONA:::
+SELECT * FROM persona;
+# Operador Sindical : Union... sin duplicados :: NO DEJA DUPLICADOS, SELECT DISTINCT...
+SELECT * FROM persona
+UNION
+SELECT * FROM persona
+;
 
-
-# Operador Sindical : Union... sin duplicados
-
-
-# Union ALL
-
+# Union ALL --- Sí deja todos los valores de ambas tablas (	NO ELIMINA DUPLICADOS )
+SELECT * FROM persona
+UNION ALL
+SELECT * FROM persona;
 
 # Union. Misma cantidad de columnas, diferentes columnas
-
+SELECT id_persona as id_general, nombre FROM persona
+UNION all
+SELECT id_venta, fecha_venta FROM ventas;
 
 # Consulta ANIDADA
+SELECT * FROM ( 
+	SELECT id_persona, nombre FROM persona
+    ) as p_reducido;
+    
+# CONSULTA ANIDADA: PEJ: MOSTRAR LA ÚLTIMA.
+SELECT * FROM (	
+    SELECT * FROM ventas ORDER BY id_venta desc limit 1
+    ) as ultima_venta;
 
-## Muestra a las personas QUE NO TENGAN VENTAS.
+## Muestra a las personas QUE NO TENGAN VENTAS. # QANIDADA, SUBQ, SUBCONSULTA
+SELECT * FROM persona
+WHERE id_persona NOT IN (SELECT id_persona FROM ventas);
 
 # Creación de vistas y tablas respaldos
 
